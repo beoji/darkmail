9afe7d2d1364d48cc473e41af5dc7a702db5237d
BA <b.bohdzian@gmail.com>
2020-06-11 18:38:19 +0200

    merged

--- src/functions.py
+++
@@ -1,7 +0,0 @@
-def input_only_nums(event):
-    keycode = event.GetKeyCode()
-    only_nums = list(range(48, 58))
-    only_nums.append(8)
-    only_nums.append(9)
-    if keycode in only_nums:
-        event.Skip()
